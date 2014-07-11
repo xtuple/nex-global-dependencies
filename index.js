4a629ffb290c7a6eff43f8699fe8c2c5cad893f8
Travis Webb <me@traviswebb.com>
2014-07-10 20:55:40 -0400

    added colors, bundled dependencies
    - use this.field

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var _ = require('lodash');
 var nex = require('nex-api');
 var proc = require('child_process');
 var log = require('npmlog');
+var colors = require('colors');
 
 var handler = module.exports = new nex.Handler('globalDependencies');
 
@@ -11,18 +12,18 @@ var handler = module.exports = new nex.Handler('globalDependencies');
  * @override
  */
 handler.do = function (pkg) {
-  _.each(pkg.globalDependencies, function (version, name) {
+  _.each(pkg[this.field], function (version, name) {
     log.info('npm install -g', nex.id(version, name));
     proc.spawnSync('npm', [ 'install', '-g', nex.id(version, name) ]);
-  });
+  }, this);
 };
 
 /**
  * @override
  */
 handler.undo = function (pkg) {
-  _.each(pkg.globalDependencies, function (version, name) {
+  _.each(pkg[this.field], function (version, name) {
     log.info('npm uninstall', nex.id(version, name));
     proc.spawnSync('npm', [ 'uninstall', '-g', nex.id(version, name) ]);
-  });
+  }, this);
 };
